99fdad908c57460d7f3436d2335d23e0d2e66327
Hasan Mahmud <1059hasan@gmail.com>
2022-06-15 18:25:01 +0600

    Age Jodi Jantam

--- main.py
+++ main.py
@@ -36,3 +36,40 @@ my_function("Sweden")
 my_function("India")
 my_function()
 my_function("Brazil")
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+def myfunction():
+  pass
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+x = lambda a : a + 10
+print(x(5))
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
